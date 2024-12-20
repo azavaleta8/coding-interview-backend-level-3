cd971bb8247f055fa2ec22b982c6b0a9c27d30a2
abelzavaleta <abel@origin.63>
2024-12-20 06:09:29 -0400

    deploy setting

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ mongoose.set('strictQuery', false);
 // ENV variables configuration
 const NODE_ENV: string = process.env.NODE_ENV || 'dev';
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
-const HOST: string = process.env.RENDER_EXTERNAL_HOSTNAME || 'localhost';
+const HOST: string = process.env.RENDER_EXTERNAL_URL || 'localhost';
 const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
@@ -56,10 +56,15 @@ export const startServer = async (): Promise<void> => {
         await connectToDatabase();
 
         const app = await createApp();
-        app.listen(PORT, HOST, () => {
-            console.log(`Server running on http://${HOST}:${PORT} in ${NODE_ENV} mode`);
-            console.log(`Swagger docs are available at  http://${HOST}:${PORT}/api-docs`);
-        });
+        app.listen(PORT, () => {
+			if (NODE_ENV === 'production') {
+                console.log(`Server running on ${HOST} in ${NODE_ENV} mode`);
+				console.log(`Swagger docs are available at ${HOST}/api-docs`);
+			} else {
+				console.log(`Server running on http://${HOST}:${PORT} in ${NODE_ENV} mode`);
+				console.log(`Swagger docs are available at http://${HOST}:${PORT}/api-docs`);
+			}
+		});
     } catch (error) {
         console.error('Error starting the server:', error);
         process.exit(1);
