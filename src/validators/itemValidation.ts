2bcc718d2dad01fc048013e5ac2d586e9bbeceb9
abelzavaleta <abel@origin.63>
2025-01-14 15:00:51 -0400

    Validation formatter

--- src/validators/itemValidation.ts
+++ src/validators/itemValidation.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
-import { body, param, validationResult } from 'express-validator';
+import { body, FieldValidationError, param, Result, ValidationError, validationResult } from 'express-validator';
+import { UnknownFieldInstance } from 'express-validator/lib/base';
 import { StatusCodes } from 'http-status-codes';
 
 export const validatePOST = [
@@ -7,16 +8,25 @@ export const validatePOST = [
         .notEmpty().withMessage('Item name is required')
         .isString().withMessage('Item name must be a string'),
     body('price')
-        .isFloat({ gt: 0 }).withMessage('Price must be a positive number'),
+        .notEmpty().withMessage('Field "price" is required')
+        .isFloat({ gt: 0 }).withMessage('Field "price" cannot be negative'),
     async (req: Request, res: Response, next: NextFunction): Promise<void> => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
+            res.status(StatusCodes.BAD_REQUEST).json({ errors: errorParser(errors.array()) });
             return;
         }
         next();
     },
 ];
+
+const errorParser = (errors: ValidationError[]) => {
+    const e : FieldValidationError  = errors[0] as FieldValidationError;
+    return [{
+        message: e.msg,
+        field: e.path,
+    }];
+};
 
 export const validateItemId = [
     param('id')
